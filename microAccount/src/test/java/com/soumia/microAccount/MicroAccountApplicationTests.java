41dc3ff519bb524f985ebb7be03fc8641f36917f
soumia <soumiaoukhira66@gmail.com>
2024-10-24 19:05:53 +0100

    initial commit

---
+++ microAccount/src/test/java/com/soumia/microAccount/MicroAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soumia.microAccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ microCustomer/src/main/java/com/soumia/microCustomer/MicroCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.soumia.microCustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroCustomerApplication.class, args);
+	}
+
+}
